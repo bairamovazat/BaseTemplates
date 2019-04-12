9aa008e7e1601b499e8c70e48633ae883068354b
bairamovazat <Bairamovazat@gmail.com>
2019-04-12 18:52:18 +0300

    add full spring boot template

--- SpringWebAppConfigExample/src/main/java/ru/azat/security/SecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-package ru.azat.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    public SecurityInitializer() {
-        super(SecurityConfig.class);
-    }
-}
